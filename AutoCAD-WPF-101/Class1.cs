08e9fa550a531cdb20f1a3db43ab08911ad71707
Gangula2 <gangulaanwesh@gmail.com>
2022-08-15 11:16:53 +0530

    Created a New Visual Studio project + added references

---
+++ AutoCAD-WPF-101/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoCAD_WPF_101
+{
+    public class Class1
+    {
+    }
+}
